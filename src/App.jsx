a6a7a048bf36f260b3a87c97a6b8a8115cc0e6ce
Mez-A <dimoabr@gmail.com>
2022-07-13 15:42:15 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>MEZJob Manager</h2>
+      p
+    </div>
+  )
+}
+
+export default App
