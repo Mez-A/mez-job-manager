f067fb4a1570edde7f670e71e2a6752f0cf34735
Mez-A <dimoabr@gmail.com>
2022-07-13 16:08:44 +0200

    fetch and show jobs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import './App.scss'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.scss";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 function App() {
+    const [jobSources, setJobSources] = useState([]);
 
-  return (
-    <div className="App">
-      <h2>MEZJob Manager</h2>
-      p
-    </div>
-  )
+    useEffect(() => {
+        (async () => {
+            setJobSources((await axios.get(backendUrl)).data);
+        })();
+    }, []);
+
+    return (
+        <div className="App">
+            <h2>MEZ Job Manager</h2>
+            <p>There are {jobSources.length} job sources.</p>
+            <ul>
+              {jobSources.map((jobSource, i) => {
+              return (
+                <li key={i}>{jobSource.name}</li>
+              )
+            })}
+            </ul>
+            
+        </div>
+    );
 }
 
-export default App
+export default App;
