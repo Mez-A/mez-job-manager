64440a4e22520d9b1ce6147f21462a0856942241
Mez-A <dimoabr@gmail.com>
2022-07-14 17:35:00 +0200

    login before jwt

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,89 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.scss";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
+// const backend_base_url = import.meta.env.VITE_BACKEND_URL;
+const backend_base_url = 'http://localhost:9000'
 
 function App() {
     const [jobSources, setJobSources] = useState([]);
+    const [currentUser, setCurrentUser] = useState({});
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const userIsLoggedIn = () => {
+        return Object.keys(currentUser).length > 0;
+    };
+
+    const getJobSources = async () => {
+        setJobSources(
+            (await axios.get(backend_base_url + "/job-sources")).data
+        );
+    };
 
     useEffect(() => {
-        (async () => {
-            setJobSources((await axios.get(backendUrl)).data);
-        })();
+        if (userIsLoggedIn()) {
+            getJobSources();
+        }
     }, []);
+
+    const handleLoginButton = async (e) => {
+        e.preventDefault();
+        const response = await fetch(backend_base_url + "/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password }),
+        });
+        setUsername("");
+        setPassword("");
+        if (response    .ok) {
+            const data = await response.json();
+            console.log(data);
+            getJobSources();
+            setCurrentUser(data.username);
+        } else {
+            setMessage("Bad Login");
+        }
+    };
 
     return (
         <div className="App">
             <h2>MEZ Job Manager</h2>
-            <p>There are {jobSources.length} job sources.</p>
-            <ul>
-              {jobSources.map((jobSource, i) => {
-              return (
-                <li key={i}>{jobSource.name}</li>
-              )
-            })}
-            </ul>
-            
+            {userIsLoggedIn() ? (
+                <>
+                    <p>There are {jobSources.length} job sources.</p>
+                    <ul>
+                        {jobSources.map((jobSource, i) => {
+                            return <li key={i}>{jobSource.name}</li>;
+                        })}
+                    </ul>
+                </>
+            ) : (
+                <form className="login">
+                    <div className="row">
+                        username:
+                        <input
+                            type="text"
+                            onChange={(e) => setUsername(e.target.value)}
+                            value={username}
+                        />
+                    </div>
+                    <div className="row">
+                        password:
+                        <input
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                        />
+                    </div>
+                    <div className="row">
+                        <button type="button" onClick={handleLoginButton} >
+                            Login
+                        </button>
+                    </div>
+                    <div className="row">{message}</div>
+                </form>
+            )}
         </div>
     );
 }
